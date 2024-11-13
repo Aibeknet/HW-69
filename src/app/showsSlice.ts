7e341bc393ff49cf4474d4a77265567b9543d7ff
aibeknet <aaskarov0690.kg@gmail.com>
2024-11-13 23:34:29 +0600

    Add Redux slice for fetching shows and show info.

---
+++ src/app/showsSlice.ts
@@ -0,0 +1,73 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+interface Show {
+  id: number;
+  name: string;
+  premiered: string;
+  summary: string;
+  image?: { original: string };
+}
+
+interface ShowsState {
+  searchShows: Show[];
+  showInfo: Show | null;
+  isLoading: boolean;
+  error: boolean;
+}
+
+const initialState: ShowsState = {
+  searchShows: [],
+  showInfo: null,
+  isLoading: false,
+  error: false,
+}
+
+export const fetchShows = createAsyncThunk(
+  'shows/fetchShows',
+  async (term: string): Promise<Show[]> => {
+    const response = await axios.get('http://api.tvmaze.com/search/shows?q=${term}');
+    return response.data.map((item: {show: Show}) => item.show)
+  }
+);
+
+export const fetchShowInfo = createAsyncThunk(
+  'shows/fetchShowInfo',
+  async (id: number): Promise<Show> => {
+    const response = await axios.get(`http://api.tvmaze.com/shows/${id}`);
+    return response.data;
+  }
+);
+
+const showsSlice = createSlice({
+  name: 'shows',
+  initialState,
+  reducers: {
+    resetShowInfo: (state) => {
+      state.showInfo = null;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchShows.pending, (state) => {
+        state.isLoading = true;
+        state.error = false;
+      })
+      .addCase(fetchShows.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.searchShows = action.payload;
+        state.error = false;
+      })
+      .addCase(fetchShows.rejected, (state) => {
+        state.isLoading = false;
+        state.error = true;
+      })
+      .addCase(fetchShowInfo.fulfilled, (state, action) => {
+        state.showInfo = action.payload;
+      });
+  },
+});
+
+export const { resetShowInfo } = showsSlice.actions;
+export default showsSlice.reducer;
